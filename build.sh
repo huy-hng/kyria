fed17d24bb0f904ebe27ab11d3a6bc9f2a2f4527
Huy <huy@hoang.dev>
2023-04-09 20:16:38 +0200

    .

--- build.sh
+++ build.sh
@@ -2,16 +2,14 @@ cd /home/huy/repositories/zmk/app
 
 
 left() {
-	west build -p -d build/left -b nice_nano_v2 -- -DSHIELD=kyria_rev3_left -DZMK_CONFIG="/home/huy/repositories/zmk-config/config"
-	# west build -p -d build/left -b nice_nano_v2 -- -DSHIELD=kyria_rev3_left
-	# west build -d build/left
+	# west build -p -d build/left -b nice_nano_v2 -- -DSHIELD=kyria_rev3_left -DZMK_CONFIG="/home/huy/repositories/zmk-config/config"
+	west build -d build/left
 	cp ./build/left/zephyr/zmk.uf2  /run/media/huy/NICENANO$1/zmk.uf2
 }
 
 right() {
-	west build -p -d build/right -b nice_nano_v2 -- -DSHIELD=kyria_rev3_right -DZMK_CONFIG="/home/huy/repositories/zmk-config/config"
-	# west build -p -d build/right -b nice_nano_v2 -- -DSHIELD=kyria_rev3_right
-	# west build -d build/right
+	# west build -p -d build/right -b nice_nano_v2 -- -DSHIELD=kyria_rev3_right -DZMK_CONFIG="/home/huy/repositories/zmk-config/config"
+	west build -d build/right
 	cp ./build/right/zephyr/zmk.uf2  /run/media/huy/NICENANO$1/zmk.uf2
 }
 
