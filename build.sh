6acc681a20e38de3e74a2bf7bda11c91da73b9cb
Huy <huy@hoang.dev>
2023-05-25 22:52:25 +0200

    fix left right build bug

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@ python ./generate_includes.py
 
 cd /home/huy/repositories/zmk/app
 config_folder="/home/huy/repositories/zmk-config/config"
-board_info="nice_nano_v2 -- -DSHIELD=kyria_rev3_left -DZMK_CONFIG=$config_folder"
 
 side() {
 	# west build -p -d build/$1 -b $board_info
+	board_info="nice_nano_v2 -- -DSHIELD=kyria_rev3_$1 -DZMK_CONFIG=$config_folder"
 	west build -d build/$1 -b $board_info
+	# west build -d build/$1 -b nice_nano_v2 -- -DSHIELD=kyria_rev3_$1 -DZMK_CONFIG=$config_folder
 
 	cp ./build/$1/zephyr/zmk.uf2  /run/media/huy/NICENANO$2/zmk.uf2
 	# cp ./build/$1/zephyr/zmk.uf2  /mnt/chromeos/removable/NICENANO$1/zmk.uf2
