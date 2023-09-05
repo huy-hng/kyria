73ec5d84bef940c718ece384ab486b2d2794d346
Huy <huy@hoang.dev>
2023-09-05 16:37:41 +0200

    formatting

--- scripts/create_combos.py
+++ scripts/create_combos.py
@@ -11,7 +11,12 @@ COMBOS_DIRECTORY = '../combos/'
 SAME_HAND_TIMEOUT = 40
 OPPOSITE_HAND_TIMEOUT = 45
 LEFT_SIDE_CODES = [0, 1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 24, 25, 26, 27, 28, 29, 30, 31, 40, 41, 42, 43, 44,]  # noqa
-RIGHT_SIDE_CODES = [6,	7,	8,	9, 10, 11, 18, 19, 20, 21, 22, 23, 32, 33, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49]	# noqa
+RIGHT_SIDE_CODES = [
+	         6,  7,  8,  9, 10, 11,
+	        18, 19, 20, 21, 22, 23,
+	32, 33, 34, 35, 36, 37, 38, 39,
+	45, 46, 47, 48, 49
+] # noqa
 
 
 class ComboFile(NamedTuple):
